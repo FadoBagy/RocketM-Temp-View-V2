1d298afa8ecddfd2cb3e4fbdf4518a07e99fe9e9
FadoBagy <mlados43@gmail.com>
2024-11-10 15:12:29 +0200

    Fixed all sections; refactored nav

--- src/functionalities.js
+++ src/functionalities.js
@@ -128,8 +128,6 @@ export function teamCardsClickToggle() {
 export function scrollToSectionWithOffset() {
     document.querySelectorAll('.navbar-link').forEach(link => {
         link.addEventListener('click', function (event) {
-            event.preventDefault();
-
             const targetId = this.getAttribute('href').substring(1);
             const targetElement = document.getElementById(targetId);
 
